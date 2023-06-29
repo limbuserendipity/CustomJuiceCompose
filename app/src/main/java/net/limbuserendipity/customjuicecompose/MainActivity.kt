ca2b9a8635a1cf95e3e45be52d5e3f9915d0fba5
limbuserendipity <darksoul1910@mail.ru>
2023-06-29 21:05:12 +0700

    Init

---
+++ app/src/main/java/net/limbuserendipity/customjuicecompose/MainActivity.kt
@@ -0,0 +1,30 @@
+package net.limbuserendipity.customjuicecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import net.limbuserendipity.customjuicecompose.ui.theme.CustomJuiceComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomJuiceComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Text(
+                        text = "Hello"
+                    )
+                }
+            }
+        }
+    }
+}
