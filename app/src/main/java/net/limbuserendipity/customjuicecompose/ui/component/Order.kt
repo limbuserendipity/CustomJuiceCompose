25413fd9e9a67f381cda8dac660e81080afda367
limbuserendipity <darksoul1910@mail.ru>
2023-07-11 19:34:25 +0700

    Add Order

---
+++ app/src/main/java/net/limbuserendipity/customjuicecompose/ui/component/Order.kt
@@ -0,0 +1,28 @@
+package net.limbuserendipity.customjuicecompose.ui.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import net.limbuserendipity.customjuicecompose.ui.model.Ingredient
+
+
+@Composable
+fun Order(){
+
+}
+
